8f990fd3ca3f513ba0a02e7a54ee18f9de2fdc63
hmtmcse <hmtm.cse@gmail.com>
2021-01-12 22:12:17 +0600

    Bismillah

---
+++ README.md
@@ -0,0 +1,1 @@
+bash gradlew :pfspring-jpa-common:clean && bash gradlew :pfspring-jpa-common:build && bash gradlew :pfspring-jpa-common:bintrayUpload
---
+++ src/main/java/com/problemfighter/pfspring/jpacommon/JpaCommonScanner.java
@@ -0,0 +1,8 @@
+package com.problemfighter.pfspring.jpacommon;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.problemfighter.pfspring.jpacommon.*"})
+public class JpaCommonScanner {
+}
